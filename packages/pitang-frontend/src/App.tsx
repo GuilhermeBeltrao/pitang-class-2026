3da880186c64f97c7ce2a0a456c8fe6a74a648cb
Keven Leone <keven.santos.sz@gmail.com>
2026-03-07 16:49:51 -0300

    Add Task page

--- packages/pitang-frontend/src/App.tsx
+++ packages/pitang-frontend/src/App.tsx
@@ -1,86 +1,77 @@
 import { useState } from "react";
-import { Fragment } from "react/jsx-runtime";
 
-type HeaderProps = {
-  description?: string;
+type Task = {
+  completed: boolean;
+  id: string;
   title: string;
-  onClick: () => void;
 };
 
-function Divider() {
-  return <hr />;
-}
-
-function Header({ description, title, onClick }: HeaderProps) {
-  return (
-    <div>
-      <h1 className="text-8xl">{title}</h1>
-      <p>{description}</p>
-
-      <button onClick={onClick}>Click me</button>
-
-      <Divider />
-    </div>
-  );
-}
-
-function Counter() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-
-  return (
-    <div>
-      {counter}
-
-      <button onClick={increment}>+</button>
-    </div>
-  );
-}
-
-function GoToFileInput() {
+function Tasks() {
   const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]);
 
-  function onChange(event) {
-    setInput(event.target.value);
+  function onSaveTask() {
+    setTasks([
+      ...tasks,
+      { completed: false, id: crypto.randomUUID(), title: input },
+    ]);
+
+    setInput("");
   }
 
-  function onClickGoToFile() {
-    alert(input);
+  function completeTask({ id }: Task) {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            completed: !task.completed,
+          };
+        }
+
+        return task;
+      }),
+    );
   }
 
   return (
-    <div>
-      <h6>{input}</h6>
+    <>
+      <div>
+        <input
+          className="p-2 border-1"
+          type="text"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
 
-      <input
-        id="goToFile"
-        name="goToFile"
-        placeholder="Go to file"
-        onChange={onChange}
-        value={input}
-      ></input>
+        <button className="rounded p-2 bg-black" onClick={onSaveTask}>
+          Save
+        </button>
+      </div>
 
-      <button onClick={onClickGoToFile}>Search</button>
-    </div>
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <li
+              className={task.completed ? "line-through" : ""}
+              key={task.id}
+              onClick={() => completeTask(task)}
+            >
+              {task.title} {String(task.completed)}
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
-export default function App() {
+export default function Pomodoro() {
   return (
-    <Fragment>
-      <Header
-        title="My first component"
-        onClick={function () {
-          alert("Cliquei no header");
-        }}
-      />
+    <div className="bg-slate-900 h-screen w-screen text-white">
+      <h1>Tasks</h1>
 
-      {/* <Counter /> */}
-
-      <GoToFileInput />
-    </Fragment>
+      <Tasks />
+    </div>
   );
 }
